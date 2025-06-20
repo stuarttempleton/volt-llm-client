2819154c94da60872804c24903a446ae90f197e3
Stuart Templeton <bvrn007@gmail.com>
2025-06-20 14:37:43 -0600

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='voltllmclient',
-    version='0.1.0',
+    version='0.1.1',
     description='A lightweight client for local LLM APIs like Ollama or OpenWebUI',
     author='Voltur',
     url='https://github.com/stuarttempleton/volt-llm-client',
